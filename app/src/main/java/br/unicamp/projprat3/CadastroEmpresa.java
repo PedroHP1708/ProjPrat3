54255dd71cd97ec499c7daa9dff786e2ad0e6ea9
Pedro <pedrohpdias10@gmail.com>
2021-09-13 11:58:55 -0300

    1 commit

---
+++ app/src/main/java/br/unicamp/projprat3/CadastroEmpresa.java
@@ -0,0 +1,14 @@
+package br.unicamp.projprat3;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class CadastroEmpresa extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_cadastro_empresa);
+    }
+}
