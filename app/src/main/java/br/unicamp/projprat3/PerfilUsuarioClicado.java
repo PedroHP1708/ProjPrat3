123f693cad745a4d3e5514eaed9a5232bff88113
Pedro <pedrohpdias10@gmail.com>
2021-09-16 18:36:53 -0300

    Feitos no dia 15 e 16

---
+++ app/src/main/java/br/unicamp/projprat3/PerfilUsuarioClicado.java
@@ -0,0 +1,14 @@
+package br.unicamp.projprat3;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class PerfilUsuarioClicado extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_perfil_usuario_clicado);
+    }
+}
---
+++ app/src/main/java/br/unicamp/projprat3/TelaVaga.java
@@ -0,0 +1,14 @@
+package br.unicamp.projprat3;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class TelaVaga extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tela_vaga);
+    }
+}
