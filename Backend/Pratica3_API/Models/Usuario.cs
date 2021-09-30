081e5438b57a6a4bc6fe0ac7e238bdec5c36158b
PedroSakai <75867370+PedroSakai@users.noreply.github.com>
2021-09-30 10:21:02 -0300

    Add files via upload

---
+++ Backend/Pratica3_API/Models/Usuario.cs
@@ -0,0 +1,24 @@
+namespace Pratica3_API.Models
+{
+    public class Usuario
+    {
+        public int Id { get; set; }
+
+        public string Nome { get; set; }
+
+        public string Area { get; set; }
+
+        public string Cpf { get; set;}
+
+        public string Cidade { get; set; }
+
+        public string Email { get; set; }
+
+        public string Senha { get; set; }
+
+        public string fotoDePerfil { get; set;}
+
+        public string Descricao { get; set;}
+
+    }
+}
---
+++ Backend/Pratica3_API/Models/VagaAplicada.cs
@@ -0,0 +1,14 @@
+namespace Pratica3_API.Models
+{
+    public class VagaAplicada
+    {
+        public int Id { get; set; }
+
+        public int idVaga { get; set; }
+
+        public int idUsuario { get; set; }
+
+        public string Situacao { get; set; }
+
+    }
+}
