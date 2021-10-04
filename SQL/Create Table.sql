d2cebbd52a5e59276f8a1275eb18ac182b194409
PedroSakai <75867370+PedroSakai@users.noreply.github.com>
2021-10-04 10:15:39 -0300

    Alteração "Email" para Chave Primaria

--- SQL/Create Table.sql
+++ SQL/Create Table.sql
@@ -1,10 +1,10 @@
-drop table Empresa
+drop table Usuario
+
 create table Usuario(
-  id    int primary key identity(1,1),
+  email		   varchar(30)   primary key,
   nome		   varchar(50)   not null,
   area		   varchar(30)   not null,
   cpf		   varchar(14)   not null,
-  email		   varchar(30)   not null,
   cidade       varchar(30)   not null,
   fotoDePerfil varchar(1000) not null,
   senha		   varchar(10)   not null,
@@ -12,11 +12,10 @@ create table Usuario(
 )
 
 create table Empresa(
-  id    int primary key identity(1,1),
+  email		   varchar(30) primary key not null,
   nome		   varchar(50)   not null,
   telefone     varchar(20)   not null,
   cnpj		   varchar(20)   not null,
-  email		   varchar(30)   not null,
   endereco     varchar(50)   not null,
   fotoDePerfil varchar(1000) not null,
   senha		   varchar(10)   not null,
@@ -25,22 +24,22 @@ create table Empresa(
 
 create table Vaga(
   id    int primary key identity(1,1),
-  idEmpresa int not null,
+  emailEmpresa varchar(30) not null,
   titulo      varchar(20)   not null,
   endereco    varchar(50)   not null,
   area		  varchar(30)   not null,
   descricao	  varchar(500)  not null,
   salarioBase money not null,
-  constraint fkEmpresa foreign key(idEmpresa)
-  references Empresa(id)
+  constraint fkEmpresa foreign key(emailEmpresa)
+  references Empresa(email)
 )
 
 create table vagaAplicada(
   id int primary key identity(1,1),
   idVaga int not null,
-  idUsuario int not null,
+  emailUsuario varchar(30) not null,
   situacao varchar(10) not null,
   constraint fkVaga    foreign key(idVaga) references Vaga(id),
-  constraint fkUsuario foreign key(idUsuario) references Usuario(id)
+  constraint fkUsuario foreign key(emailUsuario) references Usuario(email)
 )
 
--- SQL/Insert.sql
+++ SQL/Insert.sql
@@ -3,29 +3,29 @@
 
 select * from Usuario
 
-insert into Usuario values   ('Pedro Sakai' , 'Enfermagem' , '103.234.987-56', 'ps@gmail.com'      , 'Campinas', '-', 'admin', 'Formado técnico...'),
-							 ('Yara Ribeiro', 'Informática', '170.850.781-99', 'ribs@gmail.com'    ,	'Sorocaba', '-', 'admin', 'Eu fiz o curso...'),
-							 ('Pedro Perez' , 'Educacional', '193.209.117-26', 'pe.perez@yahoo.com',	'Manaus'  , '-', 'admin', 'Estou cursando...')
+insert into Usuario values   ('ps@gmail.com', 'Pedro Sakai' , 'Enfermagem' , '103.234.987-56', 'Campinas', '-', 'admin', 'Formado técnico...'),
+							 ('ribs@gmail.com', 'Yara Ribeiro', 'Informática', '170.850.781-99', 'Sorocaba', '-', 'admin', 'Eu fiz o curso...'),
+							 ('pe.perez@yahoo.com', 'Pedro Perez' , 'Educacional', '193.209.117-26', 'Manaus'  , '-', 'admin', 'Estou cursando...')
 
 -- TABELA EMPRESA
 
 select * from Empresa
 
-insert into Empresa values   ('Netflix', '(19)99263-7294', 'XX.XXX.XXX/0001-XX', 'netflix@gmail.com', 'Rua X...', '-', 'net', 'A Netflix é...'        ),
-							 ('Disney' , '...'           , 'XX.XXX.XXX/0001-XX', 'disney@gmail.com' , 'Rua Y...', '-', 'dis', 'O grupo Walt Disney...')
+insert into Empresa values   ('netflix@gmail.com', 'Netflix', '(19)99263-7294', 'XX.XXX.XXX/0001-XX', 'Rua X...', '-', 'net', 'A Netflix é...'        ),
+							 ('disney@gmail.com' , 'Disney' , '...'           , 'XX.XXX.XXX/0001-XX', 'Rua Y...', '-', 'dis', 'O grupo Walt Disney...')
 
 -- TABELA VAGA
 
 select * from Vaga
 
-insert into Vaga values (2, 'Analista', 'Rua X...', 'Economica'       , 'Estamos procurando...', 1500.00),
-						(1, 'Diretor' , 'Rua Y...', 'Cinematrogafica' , 'Buscamos alguem...'   , 2000.00)
+insert into Vaga values ('disney@gmail.com' , 'Analista', 'Rua X...', 'Economica'       , 'Estamos procurando...', 1500.00),
+						('netflix@gmail.com', 'Diretor' , 'Rua Y...', 'Cinematrogafica' , 'Buscamos alguem...'   , 2000.00)
 
 -- TABELA vagaAPLICADA
 
 select * from vagaAplicada
 
-insert into vagaAplicada values (1, 1, 'Em analise'),
-								(1, 3, 'Chamada'),
-								(2, 2, 'Recusado'),
-								(2, 1, 'Em analise')
+insert into vagaAplicada values (1, 'ps@gmail.com', 'Em analise'),
+								(1, 'pe.perez@yahoo.com', 'Chamada'),
+								(2, 'ribs@gmail.com', 'Recusado'),
+								(2, 'ps@gmail.com', 'Em analise')
