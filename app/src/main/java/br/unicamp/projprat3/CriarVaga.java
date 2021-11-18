205abede7595881cf070dac0667337a2d339a63d
PedroSakai <75867370+PedroSakai@users.noreply.github.com>
2021-11-18 10:59:00 -0300

    Add files via upload

--- app/src/main/java/br/unicamp/projprat3/CriarVaga.java
+++ app/src/main/java/br/unicamp/projprat3/CriarVaga.java
@@ -33,7 +33,7 @@ public class CriarVaga extends AppCompatActivity {
         edtSalario = (EditText) findViewById(R.id.edtSalario);
 
         // vai ser apagado
-        String emailEmpresa = "netflix@email.com";
+        String emailEmpresa = "netflix@gmail.com";
 
         btnCriar.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -42,8 +42,7 @@ public class CriarVaga extends AppCompatActivity {
                 if (!edtTitulo.getText().toString().equals("") && !edtEndereco.getText().toString().equals("") && !edtArea.getText().toString().equals("") && !edtDescricao.getText().toString().equals("") && !edtSalario.getText().toString().equals("")) {
 
                     Service service = RetrofitConfig.getRetrofitInstance().create((Service.class));
-<<<<<<< HEAD
-                    VagaAInserir vaga = new VagaAInserir(emailEmpresa, edtTitulo.getText().toString(), edtEndereco.getText().toString(), edtArea.getText().toString(), Integer.parseInt(edtSalario.getText().toString()));
+                    VagaAInserir vaga = new VagaAInserir(emailEmpresa, edtTitulo.getText().toString(), edtEndereco.getText().toString(), edtArea.getText().toString(), Double.parseDouble(edtSalario.getText().toString()), edtDescricao.getText().toString());
 
 
                     Call<VagaAInserir> call = service.incluirVaga(vaga);
@@ -51,40 +50,25 @@ public class CriarVaga extends AppCompatActivity {
                     call.enqueue(new Callback<VagaAInserir>() {
                         @Override
                         public void onResponse(Call<VagaAInserir> call, Response<VagaAInserir> response) {
-=======
-                    Vaga vaga = new Vaga(emailEmpresa, edtTitulo.getText().toString(), edtEndereco.getText().toString(), edtArea.getText().toString(), Integer.parseInt(edtSalario.getText().toString()));
-                    Call<Vaga> call = service.incluirVaga(vaga);
-
-                    call.enqueue(new Callback<Vaga>() {
-                        @Override
-                        public void onResponse(Call<Vaga> call, Response<Vaga> response) {
->>>>>>> db79a9f51cf20c0b5387ce1222bf374d65cd4471
                             if (response.isSuccessful()) {
                                 Toast.makeText(CriarVaga.this, "Ocorreu um erro na requisicao", Toast.LENGTH_SHORT).show();
                             }
-                            Toast.makeText(CriarVaga.this, vaga.toString(), Toast.LENGTH_SHORT).show();
-                            //Toast.makeText(CriarVaga.this, "Vaga Criada com Sucesso!", Toast.LENGTH_SHORT).show();
-                            //Intent intent = new Intent(CriarVaga.this, PerfilEmpresa.class);
-                            //startActivity(intent);
+
+                            Toast.makeText(CriarVaga.this, "Vaga Criada com Sucesso!", Toast.LENGTH_SHORT).show();
+                            Intent intent = new Intent(CriarVaga.this, PerfilEmpresa.class);
+                            startActivity(intent);
                         }
 
                         @Override
-<<<<<<< HEAD
                         public void onFailure(Call<VagaAInserir> call, Throwable t) {
-=======
-                        public void onFailure(Call<Vaga> call, Throwable t) {
->>>>>>> db79a9f51cf20c0b5387ce1222bf374d65cd4471
 
                             Intent intent = new Intent(CriarVaga.this, TelaPrincipalEmpresa.class);
                             startActivity(intent);
                         }
                     });
 
-<<<<<<< HEAD
 
 
-=======
->>>>>>> db79a9f51cf20c0b5387ce1222bf374d65cd4471
                 }
                 else {
                     Toast.makeText(CriarVaga.this, "Preencha todos os campos", Toast.LENGTH_SHORT).show();
